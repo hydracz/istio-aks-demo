884df035483b7aa5bc0c5c43803751565fe633a5
Zhen Chen <hydracz@gmail.com>
2023-09-24 19:09:14 +0800

    optimize my demo

--- 04-bookinfo/setup_bookinfo.sh
+++ 04-bookinfo/setup_bookinfo.sh
@@ -3,6 +3,7 @@ export KUBECONFIG=../01-aks-setup/kubeconfig
 
 #setup bookinfo application
 kubectl create ns bookinfo
+#kubectl label namespace bookinfo istio-injection=enabled --overwrite
 
 envsubst < bookinfo.yaml         | kubectl apply -n bookinfo -f -
 envsubst < bookinfo-gateway.yaml | kubectl apply -n bookinfo -f -
--- 05-istio-demo/00-sidecar-injection.sh
+++ 05-istio-demo/00-sidecar-injection.sh
@@ -1,4 +1,4 @@
 export KUBECONFIG=../01-aks-setup/kubeconfig
 
 kubectl label namespace bookinfo istio-injection=enabled --overwrite
-kubectl apply -f destination-rule-all.yaml
+kubectl apply -f destination-rule-all.yaml -n bookinfo
